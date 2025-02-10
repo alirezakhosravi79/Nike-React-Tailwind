de31973fc727cdc43c93f2eef8125384df28d3e7
alirezass <alirezakhosraviss1526@gmail.com>
2025-02-11 02:09:22 +0330

    product list

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Cart from "./pages/Cart";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import { UpdateFollower } from "react-mouse-follower";
+import ProductList from "./components/ProductList";
+import Navbar2 from "./components/Navbar2";
 
 const router = createBrowserRouter([
   {
@@ -22,12 +24,58 @@ const router = createBrowserRouter([
     ),
   },
   {
+    path: "/mens",
+    element: (
+      <>
+        <Navbar2 />
+        <ProductList category="men"/>
+        
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/womens",
+    element: (
+      <>
+        <Navbar2 />
+        <ProductList category="women"/>
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/kids",
+    element: (
+      <>
+        <Navbar2 />
+        <ProductList category="kid"/>
+        <Footer />
+      </>
+    ),
+  },
+  {
     path: "/contact",
-    element: <Contact />,
+    element: (
+      <>
+        <Navbar2 />
+        <Contact /> <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/products/:productId",
+    element: <><Navbar2/>SingleProduct<Footer/></>
   },
   {
     path: "/cart",
-    element: <Cart />,
+    element: (
+      <>
+        <Navbar2 />
+        <Cart />
+        <Footer />
+      </>
+    ),
   },
 ]);
 
@@ -36,11 +84,11 @@ function App() {
     <>
       <main className="overflow-x-hidden">
         <UpdateFollower
-        mouseOptions={{
-          backgroundColor: "white",
-          zIndex: 10,
-          followSpeed: 1.5,
-        }}
+          mouseOptions={{
+            backgroundColor: "white",
+            zIndex: 10,
+            followSpeed: 1.5,
+          }}
         >
           <RouterProvider router={router} />
         </UpdateFollower>
