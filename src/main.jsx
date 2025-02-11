331b7aa88f0af2a3f9bf8e1c715ad20658cb7522
alirezass <alirezakhosraviss1526@gmail.com>
2025-02-11 04:01:10 +0330

    context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import {MouseFollower} from 'react-mouse-follower'
+import { MouseFollower } from "react-mouse-follower";
+import ShopContextProvider from "./contex/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MouseFollower />
+    <ShopContextProvider>
+      <MouseFollower />
       <App />
+    </ShopContextProvider>
   </StrictMode>
 );
