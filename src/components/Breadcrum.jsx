8befb219bd18c1dbd813156e25b72c2f28a335cb
alirezass <alirezakhosraviss1526@gmail.com>
2025-02-11 18:17:07 +0330

    cart context

--- src/components/Breadcrum.jsx
+++ src/components/Breadcrum.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { ChevronRight } from "lucide-react";
+import React from "react";
 
-function Breadcrum() {
+const Breadcrum = (props) => {
+  const { product } = props;
   return (
-    <div>Breadcrum</div>
-  )
-}
+    <div className="flex items-center md:gap-2 gap-1 px-6 md:px-0 text-[#5e5e5e] font-semibold md:text-lg capitalize mt-4 text-sm">
+      HOME <ChevronRight /> SHOP <ChevronRight /> {product.category}{" "}
+      <ChevronRight /> {product.name}
+    </div>
+  );
+};
 
-export default Breadcrum
+export default Breadcrum;
